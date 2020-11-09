be3ed71b09ad855a0f93993c42e77abbba6a55f4
Vaishtn <73684741+Vaishtn@users.noreply.github.com>
2020-11-09 18:20:14 +0530

    Add files via upload

---
+++ Odd.java
@@ -0,0 +1,17 @@
+class Odd{
+	public static void value(int num[]){
+	for(int i=0;i<5;i++){
+		
+	if(num[i]%2!=0){
+		System.out.println(num[i]+"odd number");
+	
+	}
+	}
+}
+public static void main(String args[]){
+	int num[]={2,4,7,5,8};
+	value(num);
+	
+	}
+}
+	
